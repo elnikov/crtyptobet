2d45e3fc3187c0d69a0a4da55e1250b51cda6fe2
elnikov <elnikov.s@gmail.com>
2022-05-05 18:49:25 +0300

    setBet function with tests

--- contracts/Event.sol
+++ contracts/Event.sol
@@ -5,46 +5,58 @@ pragma solidity ^0.8.0;
 contract Event {
     address public owner; 
 
-    enum State { Created, Started, Ended }
-    // The state variable has a default value of the first member, `State.created`
+    enum State { Created, Ended }
     State public state;
+
+    error InvalidResultValue();
+    enum Result { Win, Lose}
 
     string public kind;
     string public id;
-    uint public result;
-    bool public is_end;
-    uint public total_amount;
-    uint public total_bids;
-    uint public winners_count;
-    uint public losers_count;
-    uint public fee_amount;
+    
+    uint public totalAmount;
+    uint public totalBets;
+    // uint public winners_count;
+    // uint public losers_count;
+    // uint public fee_amount;
+
+    uint minBet;
 
     
-    struct Bid {
-        string event_id;
+    struct Bet {
+        address owner;
         uint amount;
-        uint result;
+        Result result;
     }
+    Bet[] public bets;
+
 
     error InvalidState();
-
     modifier inState(State state_) {
         if (state != state_)
             revert InvalidState();
         _;
     }
 
+    function setBet(Result _result) public payable inState(State.Created) {
+            require(msg.value > minBet, "Bet value must be positive");
+            
+            bets.push(Bet({
+                owner: payable(msg.sender),
+                amount: msg.value,
+                result: _result
+            }));
 
-    // mapping(address => Bid) public bids; 
-    // mapping(string => Event) public events; 
+            totalAmount += msg.value;
+            totalBets += 1;
+    }
 
     constructor(string memory _id, string memory _kind, address payable _owner) {
         owner = _owner;
         id = _id; 
         kind = _kind; 
         state = State.Created;
-
-        is_end = false;
+        minBet = 0;
     }
 
 } 
--- test/event-test.js.js
+++ test/event-test.js.js
@@ -7,11 +7,46 @@ describe("Event", function () {
   let owner;
   let addr1;
   let addr2;
+  let addr3;
+  let addr4;
   let addrs;
 
   beforeEach(async function () {
     Event = await ethers.getContractFactory("Event");
-    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+    [owner, addr1, addr2, addr3, addr4, ...addrs] = await ethers.getSigners();
+  });
+
+  describe("setBet", function () {
+    beforeEach(async function () {
+      eventContract = await Event.deploy('123', 'w1', owner.address);
+    });
+
+    it("single bet", async function () {
+      await eventContract.connect(addr1).setBet(0,{ value: ethers.utils.parseEther("0.5")})
+
+      expect(await eventContract.totalBets()).to.equal(1);
+      expect(await eventContract.totalAmount()).to.equal(ethers.utils.parseEther("0.5"));
+    });
+
+    it("multi bet", async function () {
+      await eventContract.connect(addr1).setBet(0,{ value: ethers.utils.parseEther("0.5")})
+      await eventContract.connect(addr2).setBet(1,{ value: ethers.utils.parseEther("1")})
+      await eventContract.connect(addr3).setBet(1,{ value: ethers.utils.parseEther("0.1")})
+      await eventContract.connect(addr4).setBet(1,{ value: ethers.utils.parseEther("2")})
+
+      expect(await eventContract.totalBets()).to.equal(4);
+      expect(await eventContract.totalAmount()).to.equal(ethers.utils.parseEther("3.6"));
+    });
+
+    it("zero value", async function () {
+      await expect(eventContract.connect(addr1).setBet(0,{ value: ethers.utils.parseEther("0.0")}))
+      .to.be.revertedWith('Bet value must be positive');
+    });
+
+    it("result out of range", async function () {
+      await expect(eventContract.connect(addr1).setBet(3,{ value: ethers.utils.parseEther("0.5")}))
+      .to.be.reverted
+    });
   });
 
   describe("constructor", function () {
